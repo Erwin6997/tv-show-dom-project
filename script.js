4608be7ba5fe6f23a2d56697f9c422592b6547c5
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-04-23 13:11:53 +0100

    I Level 100 in finish now ...

--- script.js
+++ script.js
@@ -2,11 +2,67 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-}
-
+};
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode, index)=>{
+    rootElem.innerHTML += `
+    <div id="${episode.airdate}"class="episode-all">
+      <p class="episode-title">${episode.name} - S0${episode.season} E0${episode.number}</p>
+      <img class="image-episode" src=${episode.image.medium}>
+      ${episode.summary}
+    </div>`    
+  });  
 }
 
 window.onload = setup;
+
+
+/*
+     this is the first Idea but i check innerHTML i got it how can put everything inside them :
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  
+  for(let i = 0 ; i < episodeList.length ; ++i){
+    console.log(episodeList.length);
+    
+    // create three element Title , Image , Info :
+    const rootDiv = document.createElement("div");
+    const rootTitle = document.createElement("p");
+    const rootImage = document.createElement("img");
+    const rootP = document.createElement("p");
+    const rootBr = document.createElement("br");
+
+
+   // show the element :
+    rootTitle.textContent = `${episodeList[i].name} - S0${episodeList[i].season} E0${episodeList[i].number}`;
+    rootImage.src = `${episodeList[i].image.medium}`; 
+    rootP.innerHTML = `${episodeList[i].summary}`;
+    //rootBR.innerHTML = ` `;
+
+    // appendChild to the div : 
+    rootElem.appendChild(rootDiv);
+    rootDiv.appendChild(rootTitle);
+    rootDiv.appendChild(rootImage);
+    rootDiv.appendChild(rootP);
+    rootDiv.appendChild(rootBr);
+
+    // create class for element :
+    rootDiv.classList.add("container")
+    rootTitle.classList.add("tittleC");
+    rootImage.classList.add("image");
+    rootP.classList.add("info");
+
+
+    console.log("each div :" + i);
+
+  }
+};
+*/
+
+
+
+
+
+
