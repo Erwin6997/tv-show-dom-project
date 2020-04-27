0ac1f2b3e6f4f78a42f02b266343a580eb17731b
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-04-27 17:02:42 +0100

    part of level 400 : petch is done ..

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 //You can edit ALL of the code here
 // level 100: function to show the all the episode :
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-  searchEpisode()
-  selectListEpisode(allEpisodes);
-
+  const listMoves = getAllShows();
+  makeMovesSelect(listMoves);
+  showMovesToEpisodeSelectList();
+  //const allEpisodes = getAllEpisodes();
+  //makePageForEpisodes(allEpisodes);
+  //searchEpisode()
+  //selectListEpisode(allEpisodes);
 };
+ 
 const rootElem = document.getElementById("root");
 
 function makePageForEpisodes(episodeList) {
+  removeScreen();
   episodeList.forEach((episode, index)=>{
     rootElem.innerHTML += `
     <div id="${episode.id}"class="episode-all">
@@ -20,13 +24,8 @@ function makePageForEpisodes(episodeList) {
       ${episode.summary}
     </div>`    
   });  
-
   
 }
-
-
-
-
 //  level 200 : function shod the search bar :
 
 let searchContain = document.getElementById("search-container");
@@ -51,23 +50,24 @@ function searchEpisode() {
 }
 
 // level 300 start to make select with all episode and E00N00:
+let episodeID = document.getElementById("episode-Dropdown-list");
 function selectListEpisode (episodes){
+  removeEpisodeList();
   episodes.forEach((episode) => {
-    let nameEpisode = document.getElementById("episode-Dropdown-list");
     let optionEpisode = document.createElement("option");
+    optionEpisode.className = "Show-episode";
     optionEpisode.value= episode.id;
     optionEpisode.innerHTML = `S${episode.season.toString()
       .padStart(2, "0")}E${episode.number.toString()
         .padStart(2, "0")} - ${episode.name}`;
-    nameEpisode.appendChild(optionEpisode);
+        episodeID.appendChild(optionEpisode);
   });
 }
 
 // show episode select on desktop :
-let episodeID = document.getElementById("episode-Dropdown-list");
 
-function showEpisodeDesktop(){
-  const episodes = getAllEpisodes();
+
+function showEpisodeDesktop(episodes){
     Array.from(episodes).forEach((episode) => {
       if (episode.id == episodeID.value){
         removeScreen();
@@ -84,23 +84,48 @@ function showEpisodeDesktop(){
    searchDisplay.innerText =`1/73 episodes`;
 }
 episodeID.addEventListener('change', showEpisodeDesktop );
- 
+
 // start level 400 and add petch() :
 
-const listMoves = getAllShows();
-//makePageForEpisodes(listMoves);
-console.log("film:"+ listMoves[3].name); 
-listMoves.forEach((moves) => {
-  let nameMoves = document.getElementById("moves-Dropdown-list");
-  let optionMoves = document.createElement("option");
-  optionMoves.value= moves.id;
-  optionMoves.innerHTML =  `${moves.name}`;
-  nameMoves.appendChild(optionMoves);
+// fetch :
+
+function makeMovesSelect(listMoves){
+ 
+  listMoves.sort((a, b) => (a.name > b.name ? 1 : -1));
+  listMoves.forEach((moves) => {
+    let optionMoves = document.createElement("option");
+    optionMoves.value= moves.id;
+    optionMoves.innerHTML = `${moves.name}`;
+    nameMoves.appendChild(optionMoves);
+  });
   
-});
+}
+let url = "https://api.tvmaze.com/shows/show-id/episodes";
+
+function FetchFunction(episodeUrl) {
+  fetch(episodeUrl)
+  .then(function (response) {
+    return response.json()
+  })
+    .then((data) => {
+      makePageForEpisodes(data);
+      selectListEpisode(data);
+      showEpisodeDesktop(data);
+      searchEpisode();
+    })
+    .catch((error) => console.log(error));
+}
+let nameMoves = document.getElementById("moves-Dropdown-list");
+nameMoves.name = "allShows";
+nameMoves.addEventListener("change", showMovesToEpisodeSelectList);
+function showMovesToEpisodeSelectList(){
+  let episodeSelect = nameMoves.value;
+  let urlNew = url.replace("show-id", episodeSelect);
+  FetchFunction(urlNew);
+}
 
 
-
+//end fetch
 
 
 // screen remover for change screen :
@@ -108,7 +133,10 @@ function removeScreen(){
   let selectEpisodeOptions = document.getElementsByClassName("episode-all");
   Array.from(selectEpisodeOptions).forEach((opt) => opt.remove());
 }
-
+function removeEpisodeList(){
+  let selectEpisodeList = document.getElementsByClassName("Show-episode");
+  Array.from(selectEpisodeList).forEach((opt) => opt.remove());
+}
 
 //link to TvMaze
 let linkTvMaze = document.createElement("a");
